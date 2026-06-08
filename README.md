8cf858808d6c80d6c472ebc381c41db9f16de21e
angelchavezinformatica <achavezg@unitru.edu.pe>
2026-06-07 21:19:27 -0500

    feat: project setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sistema Web Gestión Comercial para Vivero
+
+## Ejecutar proyecto
+
+- Instala las dependencias con `npm install`
+
+- Iniciar el servidor de desarrollo con `npm run dev`
---
+++ app/app.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+useHead({
+  htmlAttrs: { lang: "es" },
+  title: "Sistema Web de Gestión Comercial para Vivero",
+});
+</script>
+
+<template>
+  <UApp>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </UApp>
+</template>
